c1ff913ffd59a349b41faf0c3d24bfb4eb995dbe
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2023-12-15 11:29:04 -0500

    jhf

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Space_Race
 {
-    partial class Form1
+    partial class spaceracer
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,122 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(spaceracer));
+            this.Titlelable = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Finishlinelable = new System.Windows.Forms.Label();
+            this.Score1lable = new System.Windows.Forms.Label();
+            this.Score2lable = new System.Windows.Forms.Label();
+            this.Subtitlelable = new System.Windows.Forms.Label();
+            this.Astroidtimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Titlelable
+            // 
+            this.Titlelable.BackColor = System.Drawing.Color.Black;
+            this.Titlelable.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Titlelable.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titlelable.ForeColor = System.Drawing.Color.White;
+            this.Titlelable.Location = new System.Drawing.Point(176, 130);
+            this.Titlelable.Name = "Titlelable";
+            this.Titlelable.Size = new System.Drawing.Size(463, 84);
+            this.Titlelable.TabIndex = 0;
+            this.Titlelable.Text = "Titlelable";
+            this.Titlelable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(406, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 1;
+            // 
+            // Finishlinelable
+            // 
+            this.Finishlinelable.Image = ((System.Drawing.Image)(resources.GetObject("Finishlinelable.Image")));
+            this.Finishlinelable.Location = new System.Drawing.Point(0, -1);
+            this.Finishlinelable.Name = "Finishlinelable";
+            this.Finishlinelable.Size = new System.Drawing.Size(801, 23);
+            this.Finishlinelable.TabIndex = 2;
+            // 
+            // Score1lable
+            // 
+            this.Score1lable.BackColor = System.Drawing.Color.Transparent;
+            this.Score1lable.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score1lable.ForeColor = System.Drawing.Color.White;
+            this.Score1lable.Location = new System.Drawing.Point(138, 22);
+            this.Score1lable.Name = "Score1lable";
+            this.Score1lable.Size = new System.Drawing.Size(151, 23);
+            this.Score1lable.TabIndex = 4;
+            this.Score1lable.Text = "Score1lable";
+            this.Score1lable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Score2lable
+            // 
+            this.Score2lable.BackColor = System.Drawing.Color.Transparent;
+            this.Score2lable.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score2lable.ForeColor = System.Drawing.Color.White;
+            this.Score2lable.Location = new System.Drawing.Point(511, 22);
+            this.Score2lable.Name = "Score2lable";
+            this.Score2lable.Size = new System.Drawing.Size(153, 23);
+            this.Score2lable.TabIndex = 5;
+            this.Score2lable.Text = "Score2lable";
+            this.Score2lable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Subtitlelable
+            // 
+            this.Subtitlelable.BackColor = System.Drawing.Color.Black;
+            this.Subtitlelable.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Subtitlelable.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Subtitlelable.ForeColor = System.Drawing.Color.White;
+            this.Subtitlelable.Location = new System.Drawing.Point(216, 231);
+            this.Subtitlelable.Name = "Subtitlelable";
+            this.Subtitlelable.Size = new System.Drawing.Size(389, 112);
+            this.Subtitlelable.TabIndex = 6;
+            this.Subtitlelable.Text = "Subtitlelable";
+            this.Subtitlelable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Astroidtimer
+            // 
+            this.Astroidtimer.Interval = 50;
+            this.Astroidtimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // spaceracer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Controls.Add(this.Subtitlelable);
+            this.Controls.Add(this.Score2lable);
+            this.Controls.Add(this.Score1lable);
+            this.Controls.Add(this.Finishlinelable);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Titlelable);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "spaceracer";
+            this.Text = "Space Racer ";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.spaceracer_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.spaceracer_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.spaceracer_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Titlelable;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Finishlinelable;
+        private System.Windows.Forms.Label Score1lable;
+        private System.Windows.Forms.Label Score2lable;
+        private System.Windows.Forms.Label Subtitlelable;
+        private System.Windows.Forms.Timer Astroidtimer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,158 @@ using System.Windows.Forms;
 
 namespace Space_Race
 {
-    public partial class Form1 : Form
+
+
+    public partial class spaceracer : Form
     {
-        public Form1()
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+
+        List<Rectangle> astroids = new List<Rectangle>();
+
+        List<int> astroidspeed = new List<int>();
+        List<int> astroidsize = new List<int>();
+        List<SolidBrush> colours = new List<SolidBrush>();
+
+
+        string mainmenue = "waiting";
+
+        Rectangle player1 = new Rectangle(150, 200, 20, 20);
+        Rectangle player2 = new Rectangle(450, 200, 20, 20);
+        int player1score = 0;
+        int player2score = 0;
+
+        Random randgen = new Random();
+        int randValue = 0;
+
+        public spaceracer()
         {
             InitializeComponent();
+        }
+        public void GameInitialize()
+        {
+            Titlelable.Visible = false;
+            Subtitlelable.Visible = false;
+            Titlelable.Text = "";
+            Subtitlelable.Text = "";
+
+            Astroidtimer.Enabled = false;
+
+            player1score = 0;
+            player2score = 0;
+
+            astroids.Clear();
+            astroidspeed.Clear();
+            colours.Clear();
+
+            //player.X = 0;
+            //  hero.Y = this.Height - groundheight - hero.Height;
+
+            mainmenue = "running";
+        }
+
+        private void spaceracer_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+               
+                case Keys.Space:
+                    if (mainmenue == "waiting" || mainmenue == "gameover" || mainmenue == "winning")
+                    {
+                        GameInitialize();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (mainmenue == "waiting" || mainmenue == "gameover")
+                    {
+                        this.Close();
+                    }
+                    break;
+            }
+        }
+
+        private void spaceracer_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+
+        private void spaceracer_Paint(object sender, PaintEventArgs e)
+        {
+            if (mainmenue == "waiting")
+            {
+
+                Score1lable.Text = "";
+
+                Titlelable.Text = "CATCH GAME";
+                Subtitlelable.Text = "Press Space to start or Esc to exit";
+
+            }
+            else if (mainmenue == "gameover")
+            {
+
+                Score1lable.Text = $"Player 1 Score: {player1score}";
+                Score2lable.Text = $"Player 2 Score: {player2score}";
+
+                Titlelable.Text = "GAME OVER";
+                Subtitlelable.Text = "Press Space to start or Esc to exit";
+                Astroidtimer.Enabled = false;
+            }
+            else if (mainmenue == "winning")
+            {
+
+                Titlelable.Text = "GAME OVER";
+                Subtitlelable.Text = "Press Space to start or Esc to exit";
+                Astroidtimer.Enabled = false;
+            }
+            else if (mainmenue == "running")
+            {
+                //update labels
+                Score1lable.Text = $"Score: {player1score}";
+                //update labels
+                Score2lable.Text = $"Score: {player2score}";
+                //draw hero
+                //e.Graphics.FillRectangle(lightblueBrush, player);
+
+                //draw balls
+                for (int i = 0; i < astroids.Count(); i++)
+                {
+                    e.Graphics.FillEllipse(colours[i], astroids[i]);
+                }
+                Astroidtimer.Enabled = true;
+        }   }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
